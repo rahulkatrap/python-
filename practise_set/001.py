8070033d9d8114e2257722eaa523667c1d9a4b14
rahulkatrap <152742431+rahulkatrap@users.noreply.github.com>
2024-07-29 15:52:57 +0000

    practice set 1

---
+++ practise_set/001.py
@@ -0,0 +1,26 @@
+print('''Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!''')
+
+import numpy as np
+array = np.array([1,2,3,4,5])
+mean_value = np.mean(array)
+print(mean_value)
+
+import os #importing module os
+contents = os.listdir('.')
+print("this are the content of the files")
+for item in contents:
+    print(item)
+
+
+
