7dffce20c47c0c0d61a9ecc84d2dc88aa9323e67
rahulkatrap <152742431+rahulkatrap@users.noreply.github.com>
2024-07-31 12:01:01 +0000

    x

--- practise_set/002.py
+++
@@ -1,8 +0,0 @@
-#write a python code to add two number
-
-a = input("enter a number :")
-b = 3
-c = a + b
-print(c)
-
-#
--- sample /002.py
+++
@@ -1,7 +0,0 @@
-# Variable and datatypes
-a = 10
-b = 2.34
-c = "rahul"
-c = a+b
-
-print (a,b,c)
